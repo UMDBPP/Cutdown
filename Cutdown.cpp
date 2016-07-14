44147187c4c812524b6770e3f63f26fe00f3fa39
Zachary Burnett <zrb@umd.edu>
2016-07-14 18:47:51 -0400

    abstracted cutdown functions to cutdown library

--- Cutdown.cpp
+++ Cutdown.cpp
@@ -1,27 +1,9 @@
 #include <Cutdown.h>
 
-// TODO port this loop into actual library code
-void loop()
-{
-    // look for any new messages
-    read_input();
-    // if system is armed, increment the timer indicating for how long
-    if (armed_ctr > 0)
-    {
-        armed_ctr++;
-    }
-    // if the system has been armed for more than the timeout, disarm
-    if (armed_ctr > ARM_TIMEOUT)
-    {
-        disarm_system();
-    }
-    // wait
-    delay (CYCLE_DELAY);
-}
-
 char Cutdown::begin()
 {
     armed = false;
+    released = false;
     pkt_type = 0;
     bytes_read = 0;
     fcn_code = 0;
@@ -31,7 +13,6 @@ char Cutdown::begin()
     // disarm the system before we enable the pins
     disarm_system();
 
-    pinMode(TRIGGER_PIN, OUTPUT);
     pinMode(ARMED_LED_PIN, OUTPUT);
 
     Serial.begin(9600);
@@ -51,6 +32,64 @@ char Cutdown::begin()
     }
 }
 
+void Cutdown::check_input()
+{
+    // look for any new messages
+    read_input();
+
+    // if system is armed, increment the timer indicating for how long
+    if (armed_ctr > 0)
+    {
+        armed_ctr++;
+    }
+
+    // if the system has been armed for more than the timeout, disarm
+    if (armed_ctr > ARM_TIMEOUT)
+    {
+        disarm_system();
+    }
+
+    // wait
+    delay(CYCLE_DELAY);
+}
+
+void Cutdown::arm_system()
+{
+    armed = true;
+    digitalWrite(ARMED_LED_PIN, HIGH);
+
+    tlm_pos = 0;
+    tlm_pos = addIntToTlm < uint8_t > (0xAA, tlm_data, tlm_pos);
+    sendTlmMsg(TLM_ADDR, tlm_data, tlm_pos);
+
+    armed_ctr = 1;
+}
+
+void Cutdown::disarm_system()
+{
+    armed = false;
+    digitalWrite(ARMED_LED_PIN, LOW);
+    armed_ctr = -1;
+}
+
+bool Cutdown::cutdown_is_released()
+{
+    return released;
+}
+
+bool Cutdown::system_is_armed()
+{
+    return armed;
+}
+
+void Cutdown::send_release_confirmation()
+{
+    tlm_pos = 0;
+    tlm_pos = addIntToTlm < uint8_t > (0xFF, tlm_data, tlm_pos);
+    sendTlmMsg(TLM_ADDR, tlm_data, tlm_pos);
+}
+
+// private
 void Cutdown::read_input()
 {
     if ((pkt_type = readMsg(1)) == 0)
@@ -74,90 +113,30 @@ void Cutdown::read_input()
     }
 }
 
-void Cutdown::arm_system()
-{
-    armed = true;
-    digitalWrite(ARMED_LED_PIN, HIGH);
-
-    tlm_pos = 0;
-    tlm_pos = addIntToTlm < uint8_t > (0xAA, tlm_data, tlm_pos);
-    sendTlmMsg(TLM_ADDR, tlm_data, tlm_pos);
-
-    armed_ctr = 1;
-}
-
-void disarm_system()
-{
-    armed = false;
-    digitalWrite(ARMED_LED_PIN, LOW);
-    armed_ctr = -1;
-}
-
-bool Cutdown::system_is_armed()
-{
-    return armed;
-}
-
-void Cutdown::send_release_confirmation()
-{
-    // if the system is armed, fire
-    if (armed)
-    {
-        tlm_pos = 0;
-        tlm_pos = addIntToTlm < uint8_t > (0xFF, tlm_data, tlm_pos);
-        sendTlmMsg(TLM_ADDR, tlm_data, tlm_pos);
-    }
-    // disarm the system again to prevent repeated firing attempts
-    disarm_system();
-}
-
-// Private function, prints to serial
-void Cutdown::log(String message)
-{
-    byte seconds = (millis() / 1000) % 60;
-    byte minutes = seconds / 60;
-    byte hours = minutes / 60;
-
-    String time_string = "T+";
-    if (hours < 10)
-    {
-        time_string += "0";
-    }
-    time_string += String(hours) + ":";
-    if (minutes < 10)
-    {
-        time_string += "0";
-    }
-    time_string += String(minutes) + ":";
-    if (seconds < 10)
-    {
-        time_string += "0";
-    }
-    time_string += String(seconds);
-    Serial.println("[" + time_string + "] " + message);
-}
-
-// private
 void Cutdown::command_response(uint8_t _fcncode, uint8_t data[], uint8_t length)
 {
-    // process a command to arm the system
-    if (_fcncode == ARM_FCNCODE)
+    // if released already, skip processing commands to arm, disarm, or fire
+    if (!released)
     {
-        arm_system();
+        // process a command to arm the system
+        if (_fcncode == ARM_FCNCODE)
+        {
+            arm_system();
+        }
+        // process a command to disarm the system
+        else if (_fcncode == DISARM_FCNCODE)
+        {
+            disarm_system();
+        }
+        // process a command to release
+        else if (_fcncode == FIRE_FCNCODE)
+        {
+            released = true;
+        }
     }
-    // process a command to disarm the system
-    else if (_fcncode == DISARM_FCNCODE)
-    {
-        disarm_system();
-    }
-    // process a command to FIIIIRRRRREEEEEE!
-    else if (_fcncode == FIRE_FCNCODE)
-    {
-        // fire
-        fire();
-    }
+
     // process a command to report the arm status
-    else if (_fcncode == ARM_STATUS_FCNCODE)
+    if (_fcncode == ARM_STATUS_FCNCODE)
     {
         tlm_pos = 0;
         // telemetry compilation
--- Cutdown.h
+++ Cutdown.h
@@ -1,13 +1,19 @@
+/*
+ * Cutdown.h
+ * XBEE_ADDR must be defined in client code.
+ * will throw compile error otherwise
+ */
+
+#ifndef Cutdown_h
+#define Cutdown_h
+
 #include <Wire.h>
 #include <XBee.h>
 #include <CCSDS_Xbee/ccsds_xbee.h>
 
 // physical definitions
-#define TRIGGER_PIN 3
 #define ARMED_LED_PIN 13
-// XBEE_ADDR must be defined in client code, will throw compile error otherwise
-// #define XBEE_ADDR 03
-#define TLM_ADDR 02
+#define TLM_ADDR 02     // XBee channel of LINK
 #define XBEE_PAN_ID 0x0B0B
 #define ARM_FCNCODE 0x0A
 #define ARM_STATUS_FCNCODE 0x01
@@ -15,23 +21,25 @@
 #define FIRE_FCNCODE 0x0F
 
 // behavioral constants
-#define CYCLE_DELAY 100 // time between execution cycles [ms]
-#define ARM_TIMEOUT (60000/CYCLE_DELAY) // 60 * 1000 / CYCLE_DELAY
+#define CYCLE_DELAY 100     // time between execution cycles [ms]
+#define ARM_TIMEOUT (60000 / CYCLE_DELAY)
 
-// function prototypes
 class Cutdown
 {
     public:
         char begin();
-        void read_input();
+        void check_input();
         void arm_system();
         void disarm_system();
         bool system_is_armed();
+        bool cutdown_is_released();
         void send_release_confirmation();
         void log(String message);
     private:
+        void read_input();
         void command_response(uint8_t _fcn_code, uint8_t data[], uint8_t length);
         boolean armed;
+        boolean released;
         int pkt_type;
         int bytes_read;
         uint8_t incoming_bytes[100];
@@ -39,4 +47,6 @@ class Cutdown
         uint8_t tlm_pos;
         uint8_t tlm_data[1];
         int armed_ctr;    // counter tracking number of cycles system has been armed
-}
+};
+
+#endif
